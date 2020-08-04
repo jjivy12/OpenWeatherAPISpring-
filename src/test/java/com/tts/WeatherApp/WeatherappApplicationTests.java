41d17c0c7694d7e480d144c14ef09b3892dfb41e
jjivy12 <jordan.ivy12@gmail.com>
2020-08-03 23:01:52 -0400

    intial commit

---
+++ src/test/java/com/tts/WeatherApp/WeatherappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.WeatherApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
